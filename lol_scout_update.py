ab33dba466f80b767cd95f835f9212e65aa72138
ben awad <benawad97@gmail.com>
2015-10-18 17:47:01 -0500

    Temporary fix

--- lol_scout_update.py
+++ lol_scout_update.py
@@ -1,11 +1,13 @@
-#! /Library/Frameworks/Python.framework/Versions/2.7/bin/python
+#! /usr/local/bin/python2.7
 import requests
 from random import shuffle
 import time
 from decimal import *
+import sys
 
 # read config file
-f = file("config.txt", "r")
+absolute = '/Users/benawad/Documents/python_programs/little_scripts/lol-scout-updater/'
+f = file(absolute+"config.txt", "r")
 
 info = f.readlines()
 
@@ -80,21 +82,27 @@ def getMostWinsChampion(j, potential_champions):
 	highest_champ_id = -1
 	highest_wins = -1
 
-	for i in potential_champions:
-		if champion_data[i]['totalSessionsWon'] > highest_wins:
+        if len(potential_champions)==0:
+           sys.exit() 
+        try:
+	    for i in potential_champions:
+		    if champion_data[i]['totalSessionsWon'] > highest_wins:
 			highest_wins = champion_data[i]['totalSessionsWon']
 			highest_champ_id = i
+        except Exception as e:
+            sys.exit()
 
 	return highest_champ_id
 
 
-def getSummonerInfo(j):
+def getSummonerInfo(j_list):
 	potential_champions = []
 	item_builds = []
 	summoner_spells = []
 	rune_list = []
-	for i in range(len(j['matches'])):
-		participants = j['matches'][i]['participants'][0]
+	for j in j_list:
+                j = j.json()
+		participants = j['participants'][0]
 		stats = participants['stats']
 		item1 = stats.get('item0', 0)
 		item2 = stats.get('item1', 0)
@@ -106,6 +114,8 @@ def getSummonerInfo(j):
 			continue
 		if(len(participants['runes']) > 8 or len(participants['runes']) < 4):
 			continue
+                if participants['championId'] in potential_champions:
+                    continue
 		potential_champions.append(participants['championId'])
 		item_builds.append([item1, item2, item3, item4, item5, item6])
 		runes = []
@@ -135,6 +145,18 @@ def getSummonerSpellName(summoner_spell1, summoner_spell2):
 	else:
 		raise KeyError('could not connect when getting the summoner spell id for ' + str(summoner_spell1) + ' or ' + str(summoner_spell2))
 
+def get10MatchIds(j):
+    matches = []
+    count = 0
+    for i in range(len(j)):
+        curr = j['matches'][i]
+        if curr['queue'] == 'RANKED_SOLO_5x5':
+            matches.append(curr['matchId'])
+            count += 1
+        if count == 5:
+            break
+    return matches
+
 def meets_requirements(sum_name):
 	id_r = requests.get('https://' + region + '.api.pvp.net/api/lol/' + region + '/v1.4/summoner/by-name/' + sum_name + '?api_key=' + key)
 	if(id_r.status_code == 200):
@@ -145,44 +167,49 @@ def meets_requirements(sum_name):
 		sum_name_stripped = sum_name_stripped.replace(" ", "")
 		sum_id = id_j[sum_name_stripped]['id']
 
-		rg_r = requests.get('https://' + region + '.api.pvp.net/api/lol/' + region + '/v2.2/matchhistory/' + str(sum_id) + '?api_key=' + key)
-		if(rg_r.status_code == 200):
-			potential_champions, item_builds, summoner_spells, rune_list = getSummonerInfo(rg_r.json())
-			
-			if(not potential_champions):
-				return False
+                ml_r = requests.get('https://' + region + '.api.pvp.net/api/lol/' + region + '/v2.2/matchlist/by-summoner/' + str(sum_id) + '?api_key=' + key)
+                match_ids = get10MatchIds(ml_r.json())
+                match_info_json = []
+                for ids in match_ids:
+                    time.sleep(3)
+                    m_r = requests.get('https://' + region + '.api.pvp.net/api/lol/' + region + '/v2.2/match/' + str(ids) + '?api_key=' + key)
+                    try:
+                        match_info_json.append(m_r)
+                    except Exception as e:
+                        print m_r.text
+                
+	        potential_champions, item_builds, summoner_spells, rune_list = getSummonerInfo(match_info_json)
 
-			rs_r = requests.get('https://' + region + '.api.pvp.net/api/lol/' + region + '/v1.3/stats/by-summoner/' + str(sum_id) + '/ranked?api_key=' + key)
+		if(not potential_champions):
+		    return False
 
-			if(rs_r.status_code == 200):
-				rs_j = rs_r.json()
-				highest_champ_id = getMostWinsChampion(rs_j, potential_champions)
-				data = getGamesWinsLossesKDA(rs_j, highest_champ_id)
-				games = data[0]
-				wins = data[1]
-				losses = data[2]
-				kills = data[3]
-				deaths = data[4]
-				ast = data[5]
+		rs_r = requests.get('https://' + region + '.api.pvp.net/api/lol/' + region + '/v1.3/stats/by-summoner/' + str(sum_id) + '/ranked?api_key=' + key)
 
-				champion_id = highest_champ_id
-				loc = potential_champions.index(champion_id)
-				item_build = item_builds[loc]
-				summoner_spell1 = summoner_spells[loc][0]
-				summoner_spell2 = summoner_spells[loc][1]
-				rune_page = rune_list[loc]
+		if(rs_r.status_code == 200):
+		    rs_j = rs_r.json()
+		    highest_champ_id = getMostWinsChampion(rs_j, potential_champions)
+		    data = getGamesWinsLossesKDA(rs_j, highest_champ_id)
+		    games = data[0]
+		    wins = data[1]
+		    losses = data[2]
+		    kills = data[3]
+		    deaths = data[4]
+		    ast = data[5]
 
-				champ_name = getChampName(champion_id)
-				item_build_name = item_build
-				spell_name1, spell_name2 = getSummonerSpellName(summoner_spell1, summoner_spell2)
-				changeTextFile(sum_name, region, champ_name, summoner_spell1, summoner_spell2, item_build_name[0], item_build_name[1], item_build_name[2], item_build_name[3], item_build_name[4], item_build_name[5], rune_page,
-					champion_id, games, wins, losses, kills, deaths, ast)
-				return True
-			else:
-				print 'cound not connect when getting ' + sum_name + ' ranked stats'
+		    champion_id = highest_champ_id
+		    loc = potential_champions.index(champion_id)
+        	    item_build = item_builds[loc]
+		    summoner_spell1 = summoner_spells[loc][0]
+	    	    summoner_spell2 = summoner_spells[loc][1]
+		    rune_page = rune_list[loc]
 
+		    champ_name = getChampName(champion_id)
+		    item_build_name = item_build
+		    spell_name1, spell_name2 = getSummonerSpellName(summoner_spell1, summoner_spell2)
+		    changeTextFile(sum_name, region, champ_name, summoner_spell1, summoner_spell2, item_build_name[0], item_build_name[1], item_build_name[2], item_build_name[3], item_build_name[4], item_build_name[5], rune_page, champion_id, games, wins, losses, kills, deaths, ast)
+		    return True
 		else:
-			print 'could not connect when getting ' + sum_name + ' recent games'
+		    print 'cound not connect when getting ' + sum_name + ' ranked stats'
 	else:
 		print id_r.text
 		print 'could not connect when getting ' + sum_name + ' id'
